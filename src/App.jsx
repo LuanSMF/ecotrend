e6da969495ef3165dfd45488abcd8f22b51c6a06
Luan Sá Muniz <luansamunizsf@gmail.com>
2026-09-01 15:16:47 -0300

    feat:adicionado contador de quantidade

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,71 @@
-import { useState } from 'react'
-import produtos from "./data/produtos.json";
+import { useEffect,useState } from 'react'
 import './App.css'
 
 function App() {
-  const [carrinho, setCarrinho] = useState([]);
-  const [categoriaSelecionada, setCategoriaSelecionada] = useState("Todos");
 
+  const[produtos,setProdutos] = useState([]);
+  const[carregando,setCarregando] = useState(true);
+  const[erro,setErro] = useState("");
+
+  useEffect(() => {
+    async function carregaProdutos(){
+      try{
+        const resposta= await fetch("/produtos.json");
+
+        if(!resposta.ok){
+          throw new Error("Não foi possível carregar os produtos.");
+        }
+
+        const dados  = await resposta.json();
+        setProdutos(dados);
+      }catch(erroCapturado){
+        setErro(erroCapturado.message);
+      }finally{
+        setCarregando(false);
+      }
+    }
+
+    carregaProdutos();
+  },[]);
+
+  
+  const [carrinho, setCarrinho] = useState(() => {
+    const carrinhosalvo = localStorage.getItem("carrinho");
+    if(carrinhosalvo){
+      return JSON.parse(carrinhosalvo);
+    }
+    return [];
+  });
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState("Todos");
+  useEffect (() =>{
+    localStorage.setItem("carrinho",JSON.stringify(carrinho));
+  },[carrinho])
   function AdicionarAoCarrinho(produto){
-    setCarrinho([...carrinho,produto]);
+    const produtoExistente = carrinho.find ((item)=> item.id === produto.id);
+
+    if(produtoExistente){
+      const carrinhoAtualizado = carrinho.map((item) => {
+        if(item.id === produto.id){
+          return{
+            ...item,quantidade:item.quantidade +1
+          };
+        }
+
+        return item;
+      });
+
+      setCarrinho(carrinhoAtualizado);
+    } else {
+      const novoitem = {
+        ...produto,quantidade: 1
+      };
+
+      setCarrinho([...carrinho,novoitem]);
+    }
   }
+
   function removerDoCarrinho(indexRemover){
     const carrinhoAtualizado = carrinho.filter((item,index) => index !== indexRemover);
-
     setCarrinho(carrinhoAtualizado);
   }
 
@@ -23,12 +77,37 @@ function App() {
     return produto.categoria === categoriaSelecionada;
   });
 
+  const totalCarrinho = carrinho.reduce((total,item) => {
+    return total + item.preco * item.quantidade;
+  }, 0);
+
+  function adicionarMaisUm(indexAdicionar){
+    const carrinhoAtualizado = carrinho.map((item,index) =>{
+      if(index === indexAdicionar){
+        return{...item,quantidade:item.quantidade+1};
+      }
+      return item;
+    });
+    setCarrinho(carrinhoAtualizado);
+  }
+  function removerMaisUm(indexAdicionar){
+    const carrinhoAtualizado = carrinho.map((item,index) =>{
+      if(index === indexAdicionar && item.quantidade>0){
+        return{...item,quantidade:item.quantidade-1};
+      }
+      return item;
+    })
+    .filter((item) => item.quantidade>0);
+    setCarrinho(carrinhoAtualizado);
+  }
+
+
 
   return (
     <>
       <main>
         <h1>Ecotrend</h1>
-        <p>Itens no carrinho: {carrinho.length}</p>
+        <p>Itens do carrinho: {carrinho.length}</p>
         <p>Produtos sustentáveis para um futuro melhor.</p>
         <div>
           <button onClick={() => setCategoriaSelecionada("Todos")}>
@@ -61,11 +140,18 @@ function App() {
             <p>O carrinho está vazio.</p>
           ):(
             <ul>
+              <p>Total: {totalCarrinho.toLocaleString("pt-BR",{style:"currency",currency:"BRL"})}</p>
               {carrinho.map((item,index)=>(
-                <li key={`${item.id}-${index}`}>
-                  {item.nome} - R$ {item.preco}
+                <li key={item.id}>
+                {item.nome} — Quantidade: {item.quantidade}
                   <button onClick={() => removerDoCarrinho(index)}>
                     Remover
+                  </button>
+                  <button onClick={() => adicionarMaisUm(index)}>
+                    +
+                  </button>
+                  <button onClick={() => removerMaisUm(index)}>
+                    -
                   </button>
                 </li>
               ))}
